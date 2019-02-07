363a34fe380c47862a788fbb1dfaf4cd36988888
Leonid Beder <leonid.beder@gmail.com>
2019-02-07 14:36:28 +0200

    Replace sign quotes with double quotes

--- scripts/src/index.ts
+++ scripts/src/index.ts
@@ -1,14 +1,14 @@
-import * as YAML from 'yamljs';
-import * as path from 'path';
 import Web3 = require('web3');
 
 import { ABIDefinition } from 'web3/types';
+import * as YAML from "yamljs";
+import * as path from "path";
 
 // This hack is needed because the ctor is defined to receive a required param a fix is in PR:
 // https://github.com/ethereum/web3.js/pull/1292.
 const web3 = new (Web3 as any as { new(): Web3 })();
 
-const FUNCTIONS = YAML.load(path.join(__dirname, '../../functions.yml')) as { [name: string]: ABIDefinition };
+const FUNCTIONS = YAML.load(path.join(__dirname, "../../functions.yml")) as { [name: string]: ABIDefinition };
 const FUNCTION_NAME_LENGTH = 10;
 
 export interface Web3Helper {
@@ -29,7 +29,7 @@ class Web3HelperImpl implements Web3Helper {
 
     encodeMethod(method: ABIDefinition | string, params: any[]): string {
         // If method is a string - try to fetch its input from the list of known methods.
-        if (typeof method === 'string') {
+        if (typeof method === "string") {
             let methodName = method;
 
             let sha3 = web3.utils.sha3(methodName).substring(0, FUNCTION_NAME_LENGTH);
@@ -38,7 +38,7 @@ class Web3HelperImpl implements Web3Helper {
                 throw new Error(`Could not find known method '${methodName}' from known methods list!`);
             }
 
-            method.type = 'function';
+            method.type = "function";
         }
 
         return web3.eth.abi.encodeFunctionCall(method, params);
@@ -53,7 +53,7 @@ class Web3HelperImpl implements Web3Helper {
             throw new Error(`Could not find function for signature: ${signature}!`);
         }
 
-        abi.type = 'function';
+        abi.type = "function";
 
         return {
             method: abi,
@@ -68,7 +68,7 @@ class Web3HelperImpl implements Web3Helper {
     }
 
     encodeParameters(inputAbi: string[], params: any[]): string {
-        return web3.eth.abi.encodeParameters(inputAbi, params).replace('0x','');
+        return web3.eth.abi.encodeParameters(inputAbi, params).replace("0x", "");
     }
 }
 
