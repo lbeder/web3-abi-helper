0ae303dc70f90ba0b872a2ae9f65c156d9d0cc53
Leonid Beder <leonid.beder@gmail.com>
2019-10-24 20:36:17 +0100

    Update to ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { encodeFunctionCall, encodeParameters, decodeParameters } from "web3-eth-abi";
+import { decodeParameters, encodeFunctionCall, encodeParameters } from "web3-eth-abi";
 import * as Utils from "web3-utils";
 import * as functions from "./functions.json";
 
-type ABIDataTypes = "uint256" | "boolean" | "string" | "bytes" | string;
-interface ABIDefinition {
+export type ABIDataTypes = "uint256" | "boolean" | "string" | "bytes" | string;
+export interface IABIDefinition {
     constant?: boolean;
     payable?: boolean;
     stateMutability?: "pure" | "view" | "nonpayable" | "payable";
@@ -17,37 +17,37 @@ interface ABIDefinition {
 const PREFIX_LENGTH = 2;
 const FUNCTION_NAME_LENGTH = 8;
 
-export interface Web3Helper {
-    paramsToString(method: ABIDefinition, params: any[]): string;
-    encodeMethod(method: ABIDefinition | string, params: any[]): string;
-    decodeMethod(data: string): { method: ABIDefinition; params: { [key: string]: any } };
+export interface IWeb3Helper {
+    paramsToString(method: IABIDefinition, params: any[]): string;
+    encodeMethod(method: IABIDefinition | string, params: any[]): string;
+    decodeMethod(data: string): { method: IABIDefinition; params: { [key: string]: any } };
     isAddress(address: any): boolean;
-    getMethod(methodName: string): ABIDefinition;
+    getMethod(methodName: string): IABIDefinition;
     getMethodNames(): string[];
     encodeParameters(inputAbi: string[], params: any[]): string;
 }
 
-class Web3HelperImpl implements Web3Helper {
-    paramsToString(method: ABIDefinition, params: any[]): string {
+class Web3HelperImpl implements IWeb3Helper {
+    public paramsToString(method: IABIDefinition, params: any[]): string {
         let i = 0;
         return method.inputs ? method.inputs.reduce((res, input) => {
             return `${res}    ${input.name} (${input.type}): ${params[i++]}\n`;
-        }, '') : '';
+        }, "") : "";
     }
 
-    encodeMethod(method: ABIDefinition | string, params: any[]): string {
+    public encodeMethod(method: IABIDefinition | string, params: any[]): string {
         // If method is a string - try to fetch its input from the list of known methods.
         if (typeof method === "string") {
             method = this.getMethod(method);
         }
 
-        return encodeFunctionCall(method, this._encodeNumbericParameters(params));
+        return encodeFunctionCall(method, this.encodeNumbericParameters(params));
     }
 
-    decodeMethod(data: string): { method: ABIDefinition; params: { [key: string]: any }; } {
+    public decodeMethod(data: string): { method: IABIDefinition; params: { [key: string]: any }; } {
         const signature = `0x${data.substring(PREFIX_LENGTH, PREFIX_LENGTH + FUNCTION_NAME_LENGTH)}`;
 
-        let abi = (<any>functions)[signature] as ABIDefinition;
+        const abi = (functions as any)[signature] as IABIDefinition;
         if (!abi) {
             throw new Error(`Could not find function for signature: ${signature}!`);
         }
@@ -63,17 +63,17 @@ class Web3HelperImpl implements Web3Helper {
 
         return {
             method: abi,
-            params: decodedParams
+            params: decodedParams,
         };
     }
 
-    isAddress(address: any): boolean {
+    public isAddress(address: any): boolean {
         return Utils.isAddress(address);
     }
 
-    getMethod(methodName: string): ABIDefinition {
+    public getMethod(methodName: string): IABIDefinition {
         const signature = Utils.sha3(methodName).substring(0, PREFIX_LENGTH + FUNCTION_NAME_LENGTH);
-        const method = (<any>functions)[signature] as ABIDefinition;
+        const method = (functions as any)[signature] as IABIDefinition;
         if (!method) {
             throw new Error(`Could not find known method '${methodName}' from known methods list!`);
         }
@@ -82,20 +82,21 @@ class Web3HelperImpl implements Web3Helper {
         return method;
     }
 
-    getMethodNames(): string[] {
-        return (Object.entries(functions) as [string, ABIDefinition][]).map((method: [string, ABIDefinition]) => {
-            return method[1].name || '';
+    public getMethodNames(): string[] {
+        // tslint:disable-next-line max-line-length
+        return (Object.entries(functions) as Array<[string, IABIDefinition]>).map((method: [string, IABIDefinition]) => {
+            return method[1].name || "";
         });
     }
 
-    encodeParameters(inputAbi: string[], params: any[]): string {
-        return encodeParameters(inputAbi, this._encodeNumbericParameters(params)).replace("0x", "");
+    public encodeParameters(inputAbi: string[], params: any[]): string {
+        return encodeParameters(inputAbi, this.encodeNumbericParameters(params)).replace("0x", "");
     }
 
     // Convert numeric parameters to hex strings, due to https://github.com/ethereum/web3.js/issues/2077:
-    _encodeNumbericParameters(params: any[]): any[] {
+    private encodeNumbericParameters(params: any[]): any[] {
         return params.map((p: any) => Number.isFinite(p) ? Utils.numberToHex(p) : p);
     }
 }
 
-export const Web3Helper = new Web3HelperImpl() as Web3Helper;
+export const Web3Helper = new Web3HelperImpl() as IWeb3Helper;
